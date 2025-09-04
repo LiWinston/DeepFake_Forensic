72a9dd5a53832755241021b83853013aaf82c410
WL <yongchunl@student.unimelb.edu.au>
2025-09-05 02:48:25 +1000

    2

--- backend/src/main/java/com/itproject/config/MybatisPlusConfig.java
+++ backend/src/main/java/com/itproject/config/MybatisPlusConfig.java
@@ -1,6 +1,7 @@
 package com.itproject.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
@@ -22,5 +23,16 @@ public class MybatisPlusConfig {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+    
+    /**
+     * Global configuration
+     */
+    @Bean
+    public GlobalConfig globalConfig() {
+        GlobalConfig globalConfig = new GlobalConfig();
+        // Disable banner to reduce startup logs
+        globalConfig.setBanner(false);
+        return globalConfig;
     }
 }
